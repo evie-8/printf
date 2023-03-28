352ac0cce8c7babfb25d98c50e2735efd22b472f
evie-8 <neouma29@gmail.com>
2023-03-28 13:38:48 +0300

    rot_13

--- 1-functions.c
+++ 1-functions.c
@@ -11,6 +11,34 @@ int print_digit(va_list list)
 
 	return (i);
 }
+
+/**
+ * rot_13 - prints string encoded in rot13
+ * @list: contains arguments
+ * Return: length
+ */
+int rot_13(va_list list)
+{
+	char *s = va_arg(list, char *), *origin, *_rot13;
+	int i, j;
+
+	origin = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	_rot13 = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; origin[j] != '\0'; j++)
+		{
+			if (s[i] == origin[j])
+			{
+				_putchar(_rot13[j]);
+				break;
+			}
+		}
+		if (!origin[j])
+			_putchar(s[i]);
+	}
+	return (i);
+}
 /**
  * pick_func - chooses the right function
  * @c: character
@@ -26,6 +54,7 @@ int (*pick_func(char c))(va_list list)
 		{"i", print_digit},
 		{"%", print_perc},
 		{"r", print_rev},
+		{"R", rot_13},
 		{NULL, NULL}
 	};
 
--- main.h
+++ main.h
@@ -15,6 +15,8 @@ int print_rev(va_list);
 int str_len(char *);
 int print_int(int n);
 int (*pick_func(char c))(va_list);
+int rot_13(va_list);
+
 /**
  * struct prints - struct
  * @p: pointer
